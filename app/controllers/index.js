f8e6949f2bc431354889c1fc4e83a057f3376fc0
dcheley <danielcheley@hotmail.com>
2017-03-11 23:46:36 -0500

    Adding saveInvitation action to button

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,8 +5,16 @@ export default Ember.Controller.extend({
   emailAddress: '',
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  
   isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  },
 
   actualEmailAddress: Ember.computed('emailAddress', function() {
     console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
