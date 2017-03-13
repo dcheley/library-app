e6c6466b54763578dfc11dbb4eef3394439878a1
dcheley <danielcheley@hotmail.com>
2017-03-13 18:34:30 -0400

    Deleting conflicting controller

--- app/controllers/admin/seeder.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-
-  libraries: Ember.computed.alias('model.libraries'),
-  books: Ember.computed.alias('model.books'),
-  authors: Ember.computed.alias('model.authors'),
-
-});
