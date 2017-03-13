daa55f2ec608d0fef3008b39eb8ebe152d90d98e
dcheley <danielcheley@hotmail.com>
2017-03-13 19:09:44 -0400

    Creating database dummies with faker and enabling in firebase environment

--- app/models/library.js
+++ app/models/library.js
@@ -3,11 +3,26 @@ import Ember from 'ember';
 import Faker from 'faker';
 
 export default DS.Model.extend({
+  
   name: DS.attr('string'),
   address: DS.attr('string'),
   phone: DS.attr('string'),
 
   books: DS.hasMany('book'),
 
-  isValid: Ember.computed.notEmpty('name')
+  isValid: Ember.computed.notEmpty('name'),
+
+  rendomize() {
+    this.set('name', Faker.company.companyName() + ' Library');
+    this.set('address', this._fullAddress());
+    this.set('phone', Faker.phone.phoneNumber());
+
+    //If using in chain
+    return this;
+  },
+
+  _fullAddress() {
+    return `${Faker.address.streetAddress()}, ${Faker.address.city()}`;
+  }
+
 });
--- config/environment.js
+++ config/environment.js
@@ -59,7 +59,9 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV['ember-faker'] = {
+      enabled: true
+    };
   }
 
   return ENV;
