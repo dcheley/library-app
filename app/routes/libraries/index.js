5d9c2968aeeb9e913038e088521c6fcf5f477329
dcheley <danielcheley@hotmail.com>
2017-03-13 16:45:19 -0400

    Adding delete action

--- app/routes/libraries/index.js
+++ app/routes/libraries/index.js
@@ -6,4 +6,14 @@ export default Ember.Route.extend({
     return this.store.findAll('library');
   }
 
+  actions: {
+    deleteLibrary(library) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        library.destroyRecord();
+      }
+    }
+  }
+
 });
