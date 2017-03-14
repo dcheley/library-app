bf21c3d78c6048dedf369950561e252ab53334b5
dcheley <danielcheley@hotmail.com>
2017-03-13 20:37:48 -0400

    Deleting comment

--- app/controllers/admin/seeder.js
+++ app/controllers/admin/seeder.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import Faker from 'faker';
 
 export default Ember.Controller.extend({
-  
+
   libraries: Ember.computed.alias('model.libraries'),
   books: Ember.computed.alias('model.books'),
   authors: Ember.computed.alias('model.authors'),
