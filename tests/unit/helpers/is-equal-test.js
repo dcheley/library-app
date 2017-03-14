c3cc8e119b791bb27d5ae49cdd51834b06c3c762
dcheley <danielcheley@hotmail.com>
2017-03-13 23:23:05 -0400

    Deleting space

--- tests/unit/helpers/is-equal-test.js
+++ tests/unit/helpers/is-equal-test.js
@@ -1,4 +1,3 @@
-
 import { isEqual } from 'library-app/helpers/is-equal';
 import { module, test } from 'qunit';
 
@@ -9,4 +8,3 @@ test('it works', function(assert) {
   let result = isEqual([42]);
   assert.ok(result);
 });
-
