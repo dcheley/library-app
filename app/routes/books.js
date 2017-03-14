7c6079d952c86812bedbc834cbb85d0c076f959b
dcheley <danielcheley@hotmail.com>
2017-03-13 23:22:20 -0400

    Adding actions for saving relations and workaround for Firebase server

--- app/routes/books.js
+++ app/routes/books.js
@@ -50,9 +50,19 @@ export default Ember.Route.extend({
     },
 
     saveAuthor(author, book) {
+      //Firebase adapter is buggy, manually removing the previous relation
+      book.get('author').then((previousAuthor) => {
+        previousAuthor.get('books').then((previousAuthorBooks) => {
+          previousAuthorBooks.removeObject(book);
+          previousAuthor.save();
+        });
+      });
+
+      //Setup new relation
       book.set('author', author);
       author.save();
       book.save();
+      book.save().then(() => author.save());
       book.set('isAuthorEditing', false);
     },
 
@@ -66,9 +76,18 @@ export default Ember.Route.extend({
     },
 
     saveLibrary(library, book) {
+      //Again Firebase is buggy, must manually remove previous relation
+      book.get('library').then((previousLibrary) => {
+        previousLibrary.get('books').then(previousLibraryBooks) => {
+          previousLibraryBooks.removeObject(book);
+          previousLibrary.save();
+        });
+      });
+
       book.set('library', library);
       library.save();
       book.save();
+      book.save().then(() => library.save());
       book.set('isLibraryEditing', false);
     }
   }
